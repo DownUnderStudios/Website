598c6a0dc877488ebc0ec05e5047db3f0049a9d2
DownUnderStudios <77708813+DownUnderStudios@users.noreply.github.com>
2021-01-20 13:37:42 +1100

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,6 +1,45 @@
-<?php
-include 'Modules/Header.html';
-?>
+<head>
+    <meta charset="utf-8">
+
+    <!-- Always force latest IE rendering engine or request Chrome Frame -->
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Use title if it's in the page YAML frontmatter -->
+    <title>Down Under Studios</title>
+
+    <meta name="description" content="Down Under Studios is a game development & publisher home page for news and games products by us.">
+    <meta name="keywords" content="Down, Under, Studios, php, Indie, Australia, Melbourne, Victoria, Matthew, NeoMatt, Video, Game, Games, Developer, Development, Company, publisher">
+
+	<link href="/CSS/Layout.css" rel="stylesheet" type="text/css">
+</head>
+<div class="contain-to-grid">
+      <nav class="top-bar" data-topbar="">
+        <ul class="title-area">
+          <li class="name">
+            <h1><a href="/">Down Under Studios</a></h1>
+          </li>
+          <li class="toggle-topbar menu-icon">
+            <a href="#">
+              <span>Menu</span>
+            </a>
+          </li>
+        </ul>
+
+        <section class="top-bar-section">
+          <!-- Right Nav Section -->
+          <ul class="right">
+              <li class=""><a href="News.php">News</a></li>
+			  <li class=""><a href="Games.php">Games</a></li>
+			  <li class=""><a href="Faq.php">FAQs</a></li>
+			  <li class=""><a href="About.php">About</a></li>
+			  <li class=""><a href="Contact.php">Contact</a></li>
+			  
+          </ul>
+        </section>
+      </nav>
+</div>
+
 <div class="hero">
   <div class="row">
     <div class="large-12 columns">
@@ -9,10 +48,48 @@ include 'Modules/Header.html';
   </div>
 </div>
 <div id="wrapper">
-<?php
-include 'Articles/Welcome.html';
-?>
+<div class="row">
+	<div class="large-12 columns">
+		<h2>Welcome to Down Under Studios</h2>
+	</div>
 </div>
-<?php
-include 'Modules/Footer.html';
-?>
+<div class="row">
+	<div class="large-12 columns">
+		<p>
+		  You have successfully installed XAMPP on this system! Now you can start using Apache, MariaDB, PHP and other components.
+		  You can find more info in the <a href="/dashboard/faq.html">FAQs</a> section or check the <a href="/dashboard/howto.html">HOW-TO Guides</a> for getting started with PHP applications.
+		</p>
+		<p>
+		  XAMPP is meant only for development purposes. It has certain configuration settings that make it easy to develop locally but that are insecure if you want to have your installation accessible to others.
+		  If you want have your XAMPP accessible from the internet, make sure you understand the implications and you checked the <a href="/dashboard/faq.html">FAQs</a> to learn how to protect your site. Alternatively you can use <a href="https://bitnami.com/stack/wamp">WAMP</a>, <a href="https://bitnami.com/stack/mamp">MAMP</a> or <a href="https://bitnami.com/stack/lamp">LAMP</a> which are similar packages which are more suitable for production.
+		</p>
+		<p>
+		  Start the XAMPP Control Panel to check the server status.
+		</p>
+	</div>
+	
+</div>
+
+</div>
+<div class="row">
+	<div class="large-12 columns">
+		<div class="row">
+			<div class="large-8 columns">
+				<ul class="social">
+					<li class="twitter"><a href="https://twitter.com/DownUnderSTU">Follow us on Twitter</a></li>
+					<li class="facebook"><a href="https://www.facebook.com/groups/downunderstudio">Like us on Facebook</a></li>
+				</ul>
+				<ul class="inline-list">
+					<li>
+						<a href="privacy_policy.php">Privacy Policy</a></li>
+					<li>
+						
+					</li>
+				</ul>
+			</div>
+			<div class="large-4 columns">
+				<p class="text-right">Copyright (c) 2021, Down Under Studios</p>
+			</div>
+		</div>
+	</div>
+</div>
