aa273657194f68bd7dcc4f97791a2f33e45a55e1
DownUnderStudios <77708813+DownUnderStudios@users.noreply.github.com>
2021-01-20 13:31:43 +1100

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,18 @@
+<?php
+include 'Modules/Header.html';
+?>
+<div class="hero">
+  <div class="row">
+    <div class="large-12 columns">
+      <h1><img src="/images/Logo.png">Down Under Studios </h1>
+    </div>
+  </div>
+</div>
+<div id="wrapper">
+<?php
+include 'Articles/Welcome.html';
+?>
+</div>
+<?php
+include 'Modules/Footer.html';
+?>
